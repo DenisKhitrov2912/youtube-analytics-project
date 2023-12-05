b25f8c540136e7dbeae3b35985b690d3eafbba65
Khitrov <Hitrov.95@yandex.ru>
2023-12-05 12:58:08 +0300

    денис_хитров_4_1_3_дз_fix

--- src/channel.py
+++ src/channel.py
@@ -5,14 +5,16 @@ from googleapiclient.discovery import build
 
 class Channel:
 
+
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализируется id канала.
+Дальше все данные будут подтягиваться по API."""
         self.api_key = os.getenv('YT_API_KEY')
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.youtube = build('youtube', 'v3', developerKey = self.api_key)
         self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()))
-
-
+        print(json.dumps(self.youtube.channels().list(id
+    = self.channel_id, part = 'snippet, statistics').execute()))
