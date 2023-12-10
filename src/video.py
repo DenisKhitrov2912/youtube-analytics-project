a077e5fa6cd099ee882f13d3b7fd0c35ba54473b
Khitrov <Hitrov.95@yandex.ru>
2023-12-10 21:22:26 +0300

    денис_хитров_4_2_6_дз

--- src/video.py
+++ src/video.py
@@ -5,18 +5,25 @@ from googleapiclient.discovery import build
 class Video:
     def __init__(self, video_id):
         """Инициализатор Video"""
-        api_key = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey = api_key)
-        video_info = youtube.videos().list(
-            part = 'snippet,statistics',
-            id = video_id
-        ).execute()
-        video_data = video_info['items'][0]
-        self.id = video_id
-        self.title = video_data['snippet']['title']
-        self.video_url = f"https://www.youtube.com/watch?v={video_id}"
-        self.view_count = int(video_data['statistics']['viewCount'])
-        self.like_count = int(video_data['statistics']['likeCount'])
+        try:
+            api_key = os.getenv('YT_API_KEY')
+            youtube = build('youtube', 'v3', developerKey = api_key)
+            video_info = youtube.videos().list(
+                part = 'snippet,statistics',
+                id = video_id
+            ).execute()
+            video_data = video_info['items'][0]
+            self.id = video_id
+            self.title = video_data['snippet']['title']
+            self.video_url = f"https://www.youtube.com/watch?v={video_id}"
+            self.view_count = int(video_data['statistics']['viewCount'])
+            self.like_count = int(video_data['statistics']['likeCount'])
+        except IndexError:
+            self.id = video_id
+            self.title = None
+            self.video_url = None
+            self.view_count = None
+            self.like_count = None
 
 
     def __str__(self):
